5564c51af27e45518f8f32c217d2e37257eee153
Adam Keenan <adam.g.keenan@gmail.com>
2021-12-05 01:51:37 -0500

    Updated p2 solution to solve p1

--- 2021/day5/main.py
+++ 2021/day5/main.py
@@ -5,7 +5,7 @@ from collections import defaultdict
 USE_EXAMPLE = False
 PRINT_DEBUG = False
 
-map = defaultdict(int)
+# Original p1 solution
 map = {}
 with open('example.txt' if USE_EXAMPLE else 'input.txt') as file:
     for line in file:
@@ -38,36 +38,42 @@ with open('example.txt' if USE_EXAMPLE else 'input.txt') as file:
                 count += 1
     print(count)
 
-map = {}
-with open('example.txt' if USE_EXAMPLE else 'input.txt') as file:
-    for line in file:
-        x1, y1, x2, y2 = [int(x) for x in re.match('(\d+),(\d+) -> (\d+),(\d+)', line).groups()]
-        if x2 - x1 < 0:
-            xlist = list(range(x1, x2 - 1, -1))
-        else:
-            xlist = list(range(x1, x2 + 1))
-        if y2 - y1 < 0:
-            ylist = list(range(y1, y2 - 1, -1))
-        else:
-            ylist = list(range(y1, y2 + 1))
-        if len(xlist) == 1:
-            fill = x1
-        elif len(ylist) == 1:
-            fill = y1
-        for x, y in zip_longest(xlist, ylist, fillvalue=fill):
-            if x not in map:
-                map[x] = defaultdict(int)
-            map[x][y] += 1
-    count = 0
-    for x, row in map.items():
-        for y, val in row.items():
-            if val >= 2:
-                count += 1
-    if USE_EXAMPLE:
-        for y in range(10):
-            rowstr = ''
-            for x in range(10):
-                val = map[x][y]
-                rowstr += str(val) if val > 0 else '.'
-            print(rowstr)
-    print(count)
+def func(ignoreDiagonals):
+    map = {}
+    with open('example.txt' if USE_EXAMPLE else 'input.txt') as file:
+        for line in file:
+            x1, y1, x2, y2 = [int(x) for x in re.match('(\d+),(\d+) -> (\d+),(\d+)', line).groups()]
+            if ignoreDiagonals and x1 != x2 and y1 != y2:
+                continue
+            if x2 - x1 < 0:
+                xlist = list(range(x1, x2 - 1, -1))
+            else:
+                xlist = list(range(x1, x2 + 1))
+            if y2 - y1 < 0:
+                ylist = list(range(y1, y2 - 1, -1))
+            else:
+                ylist = list(range(y1, y2 + 1))
+            if len(xlist) == 1:
+                fill = x1
+            elif len(ylist) == 1:
+                fill = y1
+            for x, y in zip_longest(xlist, ylist, fillvalue=fill):
+                if x not in map:
+                    map[x] = defaultdict(int)
+                map[x][y] += 1
+        count = 0
+        for x, row in map.items():
+            for y, val in row.items():
+                if val >= 2:
+                    count += 1
+        if USE_EXAMPLE:
+            for y in range(10):
+                rowstr = ''
+                for x in range(10):
+                    val = map[x][y]
+                    rowstr += str(val) if val > 0 else '.'
+                print(rowstr)
+        print(count)
+
+func(True)
+func(False)
