cd415b719f4b6cfb8da759bd711c4a906b969581
Adam Keenan <adam.g.keenan@gmail.com>
2021-12-06 02:13:38 -0500

    Day 6 part 2

--- 2021/day6/main.py
+++ 2021/day6/main.py
@@ -19,6 +19,7 @@ class Fish:
             # spawn new fish
             newFish.append(Fish())
 
+# part 1
 with open('example.txt' if USE_EXAMPLE else 'input.txt') as file:
     for f in file.readline().strip().split(','):
         fish.append(Fish(int(f)))
@@ -29,3 +30,22 @@ with open('example.txt' if USE_EXAMPLE else 'input.txt') as file:
             fish.append(nf)
         newFish = []
     print(len(fish))
+
+# part 2
+# had to completely rethink how the data would be stored
+# just kept track of the count of each fish by days left
+fishMap = defaultdict(int)
+with open('example.txt' if USE_EXAMPLE else 'input.txt') as file:
+    for f in file.readline().strip().split(','):
+        fishMap[int(f)] += 1
+    for i in range(256):
+        newFishMap = defaultdict(int)
+        # all 0 day fish create new fish and return to 6 day
+        for f, num in fishMap.items():
+            # Move fish with days left to day-1
+            newFishMap[f - 1] += num
+        newFishMap[6] += newFishMap[-1]
+        newFishMap[8] += newFishMap[-1]
+        del newFishMap[-1]
+        fishMap = newFishMap
+    print(sum(fishMap.values()))
